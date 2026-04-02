ce7f91eb9720a964188b02f31329c769217a1c2b
backurs <arturs.backurs@gmail.com>
2026-04-02 15:53:23 -0700

    remove hard coded url

--- utils/cosmos_retriever.py
+++ utils/cosmos_retriever.py
@@ -507,7 +507,7 @@ class CombinedRetriever:
                 "Authorization": f"Bearer {self._ranker_access_token}",
                 "Content-Type": "application/json",
             }
-            url_suffix = str(CONFIG.get("ranker", {}).get("url_suffix", "dbinference.azure.com:443/inference/semanticReranking")).strip()
+            url_suffix = str(CONFIG.get("ranker", {}).get("url_suffix", "")).strip()
             url = f"https://{self._ranker_account}.{self._ranker_region}.{url_suffix}"
             max_retries = int(CONFIG.get("ranker", {}).get("max_retries", 5))
             ranker_succeeded = False
